dc41b8aa43235cfda09800c679f50aee7cf02cbe
hckang17 <hckang17@naver.com>
2023-05-08 22:53:28 +0900

    second commit

--- sugang/config/urls.py
+++ sugang/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('SugangApply/', include('SugangApply.urls')),
+    path('Sugang/', include('sugang.urls')),
 ]
